7c88f9d8980d26c95b224f3ea36e032f5b5a6603
Akshitha-tekulapally <51028417+Akshitha-tekulapally@users.noreply.github.com>
2019-05-25 22:20:04 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,31 @@
+import java.util.Date;
+
+public class Student {
+    private String name;
+    private int rollNo;
+    private Date joinedOn;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public Date getJoinedOn() {
+        return joinedOn;
+    }
+
+    public void setJoinedOn(Date joinedOn) {
+        this.joinedOn = joinedOn;
+    }
+}
